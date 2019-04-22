f782b58d0236048e4839015c0e95e0b19e531309
ecsakoc <csaba.kocsis@ericsson.com>
2019-04-23 01:14:03 +0200

    added user and hotel model, basic routing

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 
-const express = require("express")
+// parameters
+const dbUrl = 'mongodb://localhost:27017';
+
+const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-var app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+// setup DB
+mongoose.connect(dbUrl);
+mongoose.connection.on('connected', () => {
+  console.log('db connected');
+});
+mongoose.connection.on('error', () => {
+  console.log('db connection error');
+});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+app.use('/', require('./routes'));
+
+app.listen(3000, () => {
+  console.log('Server is running.');
+});
